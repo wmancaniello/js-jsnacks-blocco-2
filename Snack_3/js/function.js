004ce1869d7d1167f6610a372a39e2aa2353c7e0
wmancaniello <wmancaniello@gmail.com>
2024-04-07 12:06:44 +0200

    Snack 3 completed

---
+++ Snack_3/js/function.js
@@ -0,0 +1,13 @@
+// scrivere una funzione contaElementi() che,
+//  dato un array in input,
+//      restituisca il numero di elementi presenti nell'array.
+
+
+/**
+ * Descrizione: dato un array, conta il numero di elementi all'intero.
+ * @param {array} array
+ * @returns {number}
+ */
+function contaElementi(array) {
+    return array.length;
+}
---
+++ Snack_3/js/script.js
@@ -0,0 +1,4 @@
+const arrayToCount = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const conteggioElementi = contaElementi(arrayToCount);
+console.log(conteggioElementi);
